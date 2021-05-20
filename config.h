e0302f5ded8e12f07ee27d7e93dc4274d408018f
Osama <iosamaify@gmail.com>
2021-05-20 19:37:42 +0200

    apply alpha patch

--- config.h
+++ config.h
@@ -93,6 +93,9 @@ char *termname = "st-256color";
  */
 unsigned int tabspaces = 2;
 
+/* bg opacity */
+float alpha = 0.9;
+
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
 	/* 8 normal colors */
@@ -120,6 +123,7 @@ static const char *colorname[] = {
 	/* more colors can be added after 255 to use with DefaultXX */
 	"#2e3440", /* background */
 	"#d8dee9", /* foreground */
+	"black",
 };
 
 
@@ -128,7 +132,7 @@ static const char *colorname[] = {
  * foreground, background, cursor, reverse cursor
  */
 unsigned int defaultfg = 257;
-unsigned int defaultbg = 234; // 256
+unsigned int defaultbg = 234;
 static unsigned int defaultcs = 257;
 static unsigned int defaultrcs = 256;
 
