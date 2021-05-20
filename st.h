671df100c9f83791bfe02eaeb1da7131ff13d65d
Osama <iosamaify@gmail.com>
2021-05-20 21:42:10 +0200

    apply externalpipe patch

--- st.h
+++ st.h
@@ -84,6 +84,7 @@ void draw(void);
 void newterm(const Arg *);
 void kscrolldown(const Arg *);
 void kscrollup(const Arg *);
+void externalpipe(const Arg *);
 void printscreen(const Arg *);
 void printsel(const Arg *);
 void sendbreak(const Arg *);
